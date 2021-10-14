841f55ffa1cbdb7b483df434bd2b52a0cef1a637
VictorSRocha <63877818+VictorSRocha@users.noreply.github.com>
2021-10-14 00:36:08 -0300

    Update videos_db.py

--- videos_db.py
+++ videos_db.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
 	host='localhost',
 	user='root',
-	passwd = 'meuserver@123',
+	passwd = 'password',
 	auth_plugin='mysql_native_password',
 	database= 'videos')
 
@@ -19,7 +19,7 @@ my_cursor = mydb.cursor()
 
 query = "SELECT * FROM videos"
 
-#iserir = 'INSERT INTO videos (titulo, url, tipo, data) VALUES (%s, %s, %s, %(data)s)]'
+#iserir = 'INSERT INTO videos (titulo, url, tipo, data) VALUES (%s, %s, %s, %(data)s)'
 
 #delete = "TRUNCATE TABLE videos"
 
